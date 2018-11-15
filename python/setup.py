34b1ac56ce80e8c403eb9ae498dfa5f969612861
Sampath Herga <info@taskmonk.ai>
2018-11-15 11:31:44 +0530

    Changed setup to include utils

--- python/setup.py
+++ python/setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='taskmonksdk',
       version='0.1',
       description='SDK for taskmonk',
       url='https://gitlab.com/HComp/taskmonksdk',
-      author='prashanth',
-      author_email='prashath@test.com',
+      author='TaskMOnk',
+      author_email='info@taskmonk.ai',
       license='',
-      packages=['taskmonksdk'],
+      packages=find_packages(),
       zip_safe=False)
